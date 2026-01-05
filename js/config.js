d6968f1829c021cf14aa396a9c3612bb75791fbf
showkay12-dot <showkay12@gmail.com>
2026-01-06 00:23:24 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 // Google Apps Script Web App URL (FINAL)
-const API_URL = "https://script.google.com/macros/s/AKfycbw6psVpoElGfEn0xTeeLGnbSD6oyTGa9nCk40aAcYBuoibKqslphE_4z128ZAI8XVyzeA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyHYvicRMyODSCXQElYRTPTwkZliQ7k4tUckGaypwskaWulMd8qqgkr9A_siTf0ene9jw/exec";
+
 
 
 
